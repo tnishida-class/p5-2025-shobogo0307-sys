356543fc17382eca0f3dba8b46903b2bc5d42576
shobogo0307-sys <shobogo0307@gmail.com>
2025-11-25 05:58:39 +0900

    finalwork

--- Q2-2/sketch.js
+++ Q2-2/sketch.js
@@ -1,11 +1,25 @@
-// チェッカー
 function setup() {
-  createCanvas(200, 200);
-  const size = width / 8; // マスの一辺の長さ
+  let s = 50;
+  let d = 40;
+  createCanvas(400, 400);
+  background(255);
   noStroke();
-  for(let i = 0; i < 8; i++){
-    for(let j = 0; j < 8; j++){
-      // BLANK[1] ヒント： rectのx座標は size * i, y座標は size * j
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      let x = i * s;
+      let y = j * s;
+      if ((i + j) % 2 === 1) {
+        fill(122);
+        rect(x, y, s, s);
+      }
+      if (((i + j) % 2 === 1) && (j < 3)) {
+        fill(255, 0, 0);
+        ellipse(x + s / 2, y + s / 2, d);
+      }
+      if (((i + j) % 2 === 1) && (4 < j)) {
+        fill(0);
+        ellipse(x + s / 2, y + s / 2, d);
+      }
     }
   }
 }
--- Q4-2/sketch.js
+++ Q4-2/sketch.js
@@ -8,16 +8,20 @@ function setup(){
   for(let i = 0; i < 10; i++){
     scores[i] = random(20, 100); // 60以上100未満のランダムな数を代入
   }
-
+  console.log(scores);
   // 横線を引く
   const n = 10;
   for(let i = 0; i < n; i++){ line(0, height * i / n, width, height * i / n); }
 
   // ここからが本番
   fill(0);
-  const dx = width / scores.length;
-  let px, py; // 線を引くために一つ前の点を覚えておく変数
   for(let i = 0; i < scores.length; i++){
+  const dx = width / scores.length
+  const h = height - height * scores[i] / 100;
+  ellipse( i * dx + 20, h, 6);
+  let px = (i - 1) * dx + 20, py = height - height * scores[i - 1] / 100;// 線を引くために一つ前の点を覚えておく変数
+  line(px, py, i * dx + 20, h)
+  
     // BLANK[1]
   }
 }
